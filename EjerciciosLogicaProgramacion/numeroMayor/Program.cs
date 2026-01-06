4b0412dba5579749b4149d7f6b1f6334880a2169
Camilo Ibanez <you@example.com>
2026-01-05 22:00:30 -0500

    .

---
+++ EjerciciosLogicaProgramacion/numeroMayor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Timers;
+var response = string. Empty;
+do
+{
+    Console.Write("Ingrese Numero: ");
+    var number = int.Parse(Console.ReadLine()!);
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"El numero {number} es par");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {number} es impar.");
+    }
+    Console.WriteLine("Desea continuar? [S/N]");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S"); 
